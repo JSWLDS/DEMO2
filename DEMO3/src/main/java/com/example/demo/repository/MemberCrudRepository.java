9f3cb0ccc26aebd88e3dc0d6fec364f91c7ca11d
JSW <jswlds2@gmail.com>
2023-05-30 16:40:43 +0900

    2023-05-26_spring_insert

--- DEMO3/src/main/java/com/example/demo/repository/MemberCrudRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Employee;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MemberCrudRepository extends CrudRepository<Employee, Integer> {
-
-}
